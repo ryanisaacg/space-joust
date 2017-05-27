539c2ae74af1a530dc21f4a9f1b9eb53e4e49af3
Ryan Goldstein <ryanisaacg@gmail.com>
2017-05-26 21:38:00 -0400

    Added a tilemap to the project

--- geom.c
+++ geom.c
@@ -1,5 +1,40 @@
 #include "geom.h"
 
-_Bool overlaps_rect(Rectangle a, Rectangle b) {
+_Bool overlaps_rect(rect a, rect b) {
 	return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
+}
+
+#define VALID (x >= 0 && y >= 0 && x < map.width && y < map.height)
+
+inline static int tl_index(tilemap map, int x, int y) {
+	x /= map.tile_width;
+	y /= map.tile_height;
+	return x * map.tile_height + y;
+}
+
+tile tl_get (tilemap map, int x, int y) {
+	if(VALID) return map.buffer[tl_index(map, x, y)];
+	else return -1;
+}
+
+void tl_set (tilemap map, int x, int y, tile val) {
+	if(VALID) map.buffer[tl_index(map, x, y)] = val;
+}
+
+tile tl_first (tilemap map, int x, int y, int w, int h) {
+	int left = x / map.tile_width;
+	int top = y / map.tile_height;
+	int right = (x + w) / map.tile_width;
+	int bottom = (y + h) / map.tile_height;
+	if(left < 0 || top < 0 || right > map.width / map.tile_width || bottom > map.height / map.tile_height)
+		return -1;
+	for(int i = left; i <= right; i++)
+		for(int j = top; j <= bottom; j++)
+			if(map.buffer[i * map.tile_height + j])
+				return map.buffer[i * map.tile_height + j];
+	return 0;
+}
+
+tile tl_first_rect(tilemap m, rect r) {
+	return tl_first(m, (int)r.x, (int)r.y, (int)r.width, (int)r.height);
 }
--- geom.h
+++ geom.h
@@ -2,6 +2,21 @@
 
 typedef struct {
 	float x, y, width, height;
-} Rectangle;
+} rect;
 
-_Bool overlaps_rect(Rectangle a, Rectangle b);
+_Bool overlaps_rect(rect a, rect b);
+
+
+typedef int tile;
+typedef struct {
+	int tile_width, tile_height, width, height;
+	tile* buffer;
+} tilemap;
+
+//Get the value at the index of the tilemap
+tile tl_get(tilemap m, int x, int y);
+//Set the value at the index of the tilemap
+void tl_set(tilemap m, int x, int y, tile val);
+//Get the first value found within the square
+tile tl_first(tilemap m, int x, int y, int w, int h);
+tile tl_first_rect(tilemap, rect);
