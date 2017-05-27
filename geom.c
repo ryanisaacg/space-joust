64a98df1f8412ddf0e2d79f1c489e5734fa54c3b
Ryan Goldstein <ryanisaacg@gmail.com>
2017-05-27 08:33:00 -0400

    Switched tilemap to a flat buffer of ints

--- geom.c
+++ geom.c
@@ -4,34 +4,37 @@ _Bool overlaps_rect(rect a, rect b) {
 	return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
 }
 
-#define VALID (x >= 0 && y >= 0 && x < map.width && y < map.height)
+#define VALID (x >= 0 && y >= 0 && x < TL_WIDTH(map) && y < TL_HEIGHT(map))
 
 inline static int tl_index(tilemap map, int x, int y) {
-	x /= map.tile_width;
-	y /= map.tile_height;
-	return x * map.tile_height + y;
+	x /= TL_TILE_WIDTH(map);
+	y /= TL_TILE_HEIGHT(map);
+	return x * TL_TILE_HEIGHT(map) + y + 4;
 }
 
 tile tl_get (tilemap map, int x, int y) {
-	if(VALID) return map.buffer[tl_index(map, x, y)];
+	if(VALID) return map[tl_index(map, x, y)];
 	else return -1;
 }
 
 void tl_set (tilemap map, int x, int y, tile val) {
-	if(VALID) map.buffer[tl_index(map, x, y)] = val;
+	if(VALID) map[tl_index(map, x, y)] = val;
 }
 
 tile tl_first (tilemap map, int x, int y, int w, int h) {
-	int left = x / map.tile_width;
-	int top = y / map.tile_height;
-	int right = (x + w) / map.tile_width;
-	int bottom = (y + h) / map.tile_height;
-	if(left < 0 || top < 0 || right > map.width / map.tile_width || bottom > map.height / map.tile_height)
+	int left = x / TL_TILE_WIDTH(map);
+	int top = y / TL_TILE_HEIGHT(map);
+	int right = (x + w) / TL_TILE_WIDTH(map);
+	int bottom = (y + h) / TL_TILE_HEIGHT(map);
+	if(left < 0 || top < 0 || right > TL_WIDTH(map) / TL_TILE_WIDTH(map) || bottom > TL_HEIGHT(map) / TL_TILE_HEIGHT(map))
 		return -1;
-	for(int i = left; i <= right; i++)
-		for(int j = top; j <= bottom; j++)
-			if(map.buffer[i * map.tile_height + j])
-				return map.buffer[i * map.tile_height + j];
+	for(int i = left; i <= right; i++) {
+		for(int j = top; j <= bottom; j++) {
+			int index = i * TL_TILE_HEIGHT(map) + j + 4;
+			if(map[index])
+				return map[index];
+		}
+	}
 	return 0;
 }
 
--- geom.h
+++ geom.h
@@ -1,6 +1,10 @@
 #pragma once
 
 typedef struct {
+	float x, y;
+} vec;
+
+typedef struct {
 	float x, y, width, height;
 } rect;
 
@@ -8,11 +12,17 @@ _Bool overlaps_rect(rect a, rect b);
 
 
 typedef int tile;
-typedef struct {
-	int tile_width, tile_height, width, height;
-	tile* buffer;
-} tilemap;
-
+typedef int* tilemap;
+/*
+ * tilemap: {tile_width, tile_height, width, height, ...}
+ */
+ 
+//Get the properties of the tilemap
+#define TL_SIZE_REQUIRED(w, h) (4 + w * h)
+#define TL_WIDTH(m) m[2]
+#define TL_HEIGHT(m) m[3]
+#define TL_TILE_WIDTH(m) m[0]
+#define TL_TILE_HEIGHT(m) m[1]
 //Get the value at the index of the tilemap
 tile tl_get(tilemap m, int x, int y);
 //Set the value at the index of the tilemap
