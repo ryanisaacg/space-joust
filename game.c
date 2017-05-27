be069633c1f2b6862baf4b4337ea87d285f2ec6f
Ryan Goldstein <ryanisaacg@gmail.com>
2017-05-27 09:07:12 -0400

    Added a demonstration of tl_first and tl_slide working

--- game.c
+++ game.c
@@ -1,6 +1,7 @@
 #include "game.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "geom.h"
 
@@ -9,6 +10,12 @@ void game_loop(GPU_Target *target) {
 	SDL_Event event;
 	
 	tilemap m = tl_init(640, 480, 32, 32, malloc(sizeof(*m) * TL_SIZE_REQUIRED(640, 480)));
+	
+	tl_set(m, 0, 0, 1);
+	
+	printf("%d\n", tl_first(m, 32, 5, 10, 10));
+	vec v = tl_slide(m, (rect){35, 5, 20, 20}, (vec){ -20, 0 });
+	printf("%f:%f\n", v.x, v.y);
 	
 	done = 0;
 	while(!done) {
