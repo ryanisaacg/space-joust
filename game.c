5c704c323be9e3c4413b1a1dda1ba73faddc65b0
Ryan Goldstein <ryanisaacg@gmail.com>
2017-06-04 18:54:05 -0400

    Added a rudimentary character controller in the main loop

--- game.c
+++ game.c
@@ -1,5 +1,7 @@
 #include "au.h"
 
+#include <stdbool.h>
+
 typedef struct {
 	AU_Rectangle bounds;
 	AU_Vector speed, acceleration;
@@ -7,21 +9,50 @@ typedef struct {
 
 void game_loop(AU_Engine* eng) {
 
-	Character player;
+	Character player = {{0, 0, 32, 32}, {0, 0}, {0, 0}};
 
-	while(eng->should_continue) {
+	AU_Texture player_tex = au_load_texture(eng, "../player.png");
+
+	while (eng->should_continue) {
 		au_begin(eng);
 
-		player.acceleration = (AU_Vector) { 0, 0 };
+		player.acceleration = (AU_Vector) {
+			0, 0
+		};
+		bool moving = false;
 		//Keyboard controls
-		if(eng->current_keys[SDL_SCANCODE_W]) player.acceleration.y -= 0.1;
-		if(eng->current_keys[SDL_SCANCODE_S]) player.acceleration.y += 0.1;
-		if(eng->current_keys[SDL_SCANCODE_A]) player.acceleration.x -= 0.1;
-		if(eng->current_keys[SDL_SCANCODE_D]) player.acceleration.x += 0.1;
+		if (eng->current_keys[SDL_SCANCODE_W]) {
+			player.acceleration.y -= 0.1;
+			moving = true;
+		}
+		if (eng->current_keys[SDL_SCANCODE_S]) {
+			player.acceleration.y += 0.1;
+			moving = true;
+		}
+		if (eng->current_keys[SDL_SCANCODE_A]) {
+			player.acceleration.x -= 0.1;
+			moving = true;
+		}
+		if (eng->current_keys[SDL_SCANCODE_D]) {
+			player.acceleration.x += 0.1;
+			moving = true;
+		}
 
 		player.speed = au_geom_vec_add(player.speed, player.acceleration);
+		player.speed = au_geom_vec_cmp_clamp(player.speed, -8, 8);
+		if (!moving) {
+			if (au_geom_vec_len2(player.speed) < 2.0f) {
+				player.speed = (AU_Vector) {
+					0, 0
+				};
+			} else {
+				au_geom_vec_scl(player.speed, 0.5f);
+			}
+		}
 		player.bounds.x += player.speed.x;
 		player.bounds.y += player.speed.y;
+
+		au_draw_texture(eng, player_tex, player.bounds.x, player.bounds.y, player.bounds.width, player.bounds.height);
 
 		au_end(eng);
 	}
